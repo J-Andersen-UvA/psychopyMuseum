e52bb42fad924e8557369421240290f504e68953
J-Andersen-UvA <j.andersen@uva.nl>
2025-03-13 15:48:53 +0100

    Sound player class for playing sounds in a separate python instance. We have little time so we did it dirty. We aren't using psychopy for playing audio because it was too bugged.

---
+++ src/sound_player.py
@@ -0,0 +1,32 @@
+import subprocess
+import os
+import signal
+import time
+
+class SoundPlayer:
+    def __init__(self, python_path="C:/Users/VICON/Desktop/Code/psychopyMuseum/psychopyMuseum/psychopy_env/Scripts/python.exe"):
+        self.process = None
+        self.python_path = python_path
+
+    def play(self, sound_path=None):
+        if self.process is not None:
+            print("Sound is already playing.")
+            return
+        self.process = subprocess.Popen([self.python_path, "-c", f"from playsound import playsound; playsound(r'{sound_path}')"])
+
+    def stop(self):
+        if self.process is not None:
+            os.kill(self.process.pid, signal.SIGTERM)
+            self.process = None
+        else:
+            print("No sound is playing.")
+
+# Example usage:
+player = SoundPlayer()
+player.play(r"C:\Users\VICON\Desktop\Code\psychopyMuseum\psychopyMuseum\noise_folder\background_noise_30.wav")
+time.sleep(2)
+player.stop()
+time.sleep(1)
+player.play(r"C:\Users\VICON\Desktop\Code\psychopyMuseum\psychopyMuseum\noise_folder\background_noise_30.wav")
+time.sleep(2)
+player.stop()
